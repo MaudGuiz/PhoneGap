f3d2dfb5378c18b2353c472d780f905d2387a3da
MaudGuiz <m.rodier@guizdigital.com>
2015-10-26 15:20:37 +0100

    Import fichiers

---
+++ config.js
@@ -0,0 +1,17 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : '9',
+		wp_ws_url : 'http://phonegap.guizdigital.com/wp-appkit-api/9',
+		theme : 'my-theme',
+		version : '',
+		app_title : 'Titre',
+		debug_mode : 'off',
+		auth_key : 'uE&SuXNfUImFuW*k3NlWNWMcYvVM4DzqU3sD5E@HTw&^T*ki@GS!HFE(^t18GP8W',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
